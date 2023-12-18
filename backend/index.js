03c2e13b3d688a22d68afe068d600ebc6974e902
kixixo <olugbanmu@outlook.com>
2023-12-18 16:25:47 -0500

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,62 @@
+import express from "express";
+import mysql from "mysql2";
+import cors from "cors";
+
+const app = express()
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "mirakulous",
+  database: "crud"
+})
+
+//If authentication problem exists use code below
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'mirakulous';
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json("Hello!, you have successfully connected to the backend!")
+})
+
+app.get('/books', (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err)
+    return res.json(data);
+  })
+})
+
+app.post('/books', (req, res) => {
+  const q = "INSERT INTO books (`title`, `desc`, `price`, `cover`) VALUES (?)"
+  const values = [req.body.title, req.body.desc, req.body.price, req.body.cover];
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err)
+    return res.json("Book has been created successfully!");
+  });
+});
+
+app.put('/books/:id', (req, res) => {
+  const bookId = req.params.id;
+  const q = "UPDATE books SET `title` = ?, `description`= ?, `price`= ?, `cover` = ? WHERE id = ?"
+
+  const values = [req.body.title, req.body.desc, req.body.price, req.body.cover];
+  db.query(q, [...values, bookId], (err, data) => {
+    if (err) return res.json(err)
+    return res.json("Book has been updated successfully!");
+  });
+})
+app.delete('/books/:id', (req, res) => {
+  const bookId = req.params.id;
+  const q = "DELETE FROM books WHERE id = ?";
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.json(err)
+    return res.json("Book has been deleted successfully!");
+  });
+})
+
+app.listen(8080, () => {
+  console.log("Connected!")
+})
---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Books from "./pages/Books";
+import Add from "./pages/Add";
+import Update from "./pages/Update";
+import "./styles.css";
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/update/:id" element={<Update />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
